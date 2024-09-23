7f588ae4463365041f740615d8ecd643e883cb9c
Human0467 <mustchjo@gmail.com>
2024-09-23 10:57:21 +0100

    exercise 2 (increment / reset / get) passing

--- src/main/java/com/corndel/exercises/Counter.java
+++ src/main/java/com/corndel/exercises/Counter.java
@@ -2,9 +2,22 @@ package com.corndel.exercises;
 
 public class Counter{
 
-    int count;
+    private int count;
 
     public Counter(){
         this.count = 0;
     }
+
+    public void increment(){
+        this.count++;
+    }
+
+    public void reset(){
+        this.count = 0;
+    }
+
+    public int getCount(){
+        return this.count;
+    }
+
 }
