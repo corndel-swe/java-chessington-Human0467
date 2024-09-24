d308274fc9120798e6894b2e617400a83c67b14a
Human0467 <mustchjo@gmail.com>
2024-09-24 15:23:00 +0100

    all Bishop tests passing

--- src/main/java/com/corndel/chessington/model/pieces/Bishop.java
+++ src/main/java/com/corndel/chessington/model/pieces/Bishop.java
@@ -36,29 +36,57 @@ public class Bishop implements Piece {
   @Override
   public List<Move> getAllowedMoves(Coordinates from, Board board) {
     var allowedMoves = new ArrayList<Move>();
-      // traverse every space of the board
-      int[] possibleDiagonals = {1,-1};
-      for(int possibleDiagonal : possibleDiagonals ) {
-        int intercept = from.getRow() - possibleDiagonal*(from.getCol());
-        for (int i = 0; i <= 7; i++) {
-          for (int j = 0; j <= 7; j++) {
-            // are the points on the line?
-            if (j - (possibleDiagonal*i) - intercept == 0) {
-              System.out.println("(" + i + "," + j + ") is on the line");
-              // record how to get there
-              int colDiff = i - from.getCol();
-              int rowDiff = j - from.getRow();
-              if (!(rowDiff == 0 && colDiff == 0)) {
-                // check here if space is occupied
-                //  - by friendly? blocked
-                //  - by enemy? can take
-                allowedMoves.add(new Move(from, from.plus(rowDiff, colDiff)));
-                System.out.println(new Move(from, from.plus(rowDiff, colDiff)).toString());
-              }
-            }
-          }
+
+    String[] directions = {"UR", "DR", "UL", "DL"};
+    // loop over every allowed direction
+    for(String direction : directions){
+      Coordinates proposed = from;
+      boolean stopped = false;
+      System.out.println(direction);
+      // keep checking spaces in this direction until we get 'stopped'
+      while(!stopped){
+        // get proposed next move
+        proposed = board.getNextMove(proposed, direction);
+        // check if on board?
+        if(!board.isSpaceOnBoard(proposed)) {
+          stopped = true;
+          break;
+        } else if(board.isSpaceEmpty(proposed)){
+          allowedMoves.add(new Move(from, proposed));
+          continue;
+        } else if(board.isPieceFriendly(proposed, this.getColour())){
+          stopped = true;
+          break;
+        } else {
+          stopped = true;
+          allowedMoves.add(new Move(from, proposed));
+          break;
         }
       }
+    }
+//      // traverse every space of the board
+//      int[] possibleDiagonals = {1,-1};
+//      for(int possibleDiagonal : possibleDiagonals ) {
+//        int intercept = from.getRow() - possibleDiagonal*(from.getCol());
+//        for (int i = 0; i <= 7; i++) {
+//          for (int j = 0; j <= 7; j++) {
+//            // are the points on the line?
+//            if (j - (possibleDiagonal*i) - intercept == 0) {
+//              System.out.println("(" + i + "," + j + ") is on the line");
+//              // record how to get there
+//              int colDiff = i - from.getCol();
+//              int rowDiff = j - from.getRow();
+//              if (!(rowDiff == 0 && colDiff == 0)) {
+//                // check here if space is occupied
+//                //  - by friendly? blocked
+//                //  - by enemy? can take
+//                allowedMoves.add(new Move(from, from.plus(rowDiff, colDiff)));
+//                System.out.println(new Move(from, from.plus(rowDiff, colDiff)).toString());
+//              }
+//            }
+//          }
+//        }
+//      }
 
     return allowedMoves;
   }
--- src/test/java/com/corndel/chessington/model/pieces/BishopTest.java
+++ src/test/java/com/corndel/chessington/model/pieces/BishopTest.java
@@ -51,7 +51,7 @@ public class BishopTest {
             new Move(coords, new Coordinates(0, 7)));
   }
 
-  @Disabled
+  //@Disabled
   @Test
   public void bishopCanCaptureOpposingPiece() {
     // Arrange
@@ -69,7 +69,7 @@ public class BishopTest {
     assertThat(allowedMoves).contains(new Move(coords, opponentCoords));
   }
 
-  @Disabled
+  //@Disabled
   @Test
   public void bishopCannotMoveThroughOpposingPiece() {
     // Arrange
@@ -89,7 +89,7 @@ public class BishopTest {
             new Move(coords, new Coordinates(1, 1)), new Move(coords, new Coordinates(0, 0)));
   }
 
-  @Disabled
+  //@Disabled
   @Test
   public void bishopIsBlockedByFriendlyPiece() {
     // Arrange
