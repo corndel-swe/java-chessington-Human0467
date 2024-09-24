4354f421ad151f59320d9f9b9d9c45ac8f6be6a2
Human0467 <mustchjo@gmail.com>
2024-09-24 10:50:16 +0100

    right triangle impletements the shape interface

--- src/main/java/com/corndel/exercises/RightTriangle.java
+++ src/main/java/com/corndel/exercises/RightTriangle.java
@@ -1,6 +1,6 @@
 package com.corndel.exercises;
 
-public class RightTriangle {
+public class RightTriangle implements Shape{
   private double base;
   private double height;
 
@@ -19,5 +19,17 @@ public class RightTriangle {
 
   public double getHypotenuse() {
     return Math.sqrt(base * base + height * height);
+  }
+
+  @Override
+  public double getArea() {
+    return 0.5*this.base*this.height;
+  }
+
+  @Override
+  public double getPerimeter() {
+    return this.base + this.height +
+            Math.sqrt(Math.pow(this.base,2) +
+                    Math.pow(this.height,2));
   }
 }
