2394c58d09e4330f259fad0c6584d8aae1917cc6
Human0467 <mustchjo@gmail.com>
2024-09-26 10:20:34 +0100

    tests for Maths.max written and passing

--- src/main/java/com/corndel/exercises/MathsTests.java
+++ src/main/java/com/corndel/exercises/MathsTests.java
@@ -9,4 +9,20 @@ public class MathsTests {
     public void testPIholdsCorrectValue() {
         assertEquals(3.14, Maths.PI);
     }
+
+    @Test
+    public void maxReturnsLargestNumber() {
+        assertEquals(10, Maths.max(10, 8));
+    }
+
+    @Test
+    public void maxWorksWhenLargestNumberSecond(){
+        assertEquals(10, Maths.max(8, 10));
+        assertEquals(10, Maths.max(10,8));
+    }
+
+    @Test
+    public void maxReturnsNumberWhenBothEqual(){
+        assertEquals(6.5, Maths.max(6.5, 6.5));
+    }
 }
